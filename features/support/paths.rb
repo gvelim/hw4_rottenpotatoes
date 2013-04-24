051035dbdbcb0aa4a94959151d5399858f79b838
saasbook <saasbook@saasbook.(none)>
2013-04-24 16:44:23 -0700

    added new files

---
+++ features/support/paths.rb
@@ -0,0 +1,52 @@
+# TL;DR: YOU SHOULD DELETE THIS FILE
+#
+# This file is used by web_steps.rb, which you should also delete
+#
+# You have been warned
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
+  def path_to(page_name)
+    case page_name
+
+    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    when /^the movies page$/ then '/movies'
+
+    # Add more mappings here.
+    # Here is an example that pulls values out of the Regexp:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
+		when /[E|e]dit/
+			title = page_name.match(/"([^"]*)"/)[1]
+			movie = Movie.find_by_title(title)
+			edit_movie_path(movie)
+	  
+	  when /[d|D]etails/
+			title = page_name.match(/"([^"]*)"/)[1]
+			movie = Movie.find_by_title(title)
+			movie_path(movie)
+	  	
+	  when /Similar Movies/
+			title = page_name.match(/"([^"]*)"/)[1]
+			movie = Movie.find_by_title(title)
+			same_directors_path(:id => movie.id)
+	  
+    else
+      begin
+        page_name =~ /^the (.*) page$/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue NoMethodError, ArgumentError
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
+    end
+  end
+end
+
+World(NavigationHelpers)
---
+++ spec/movie_spec.rb
@@ -0,0 +1,11 @@
+#movie_spec.rb
+
+require 'spec_helper'
+
+describe MoviesController do
+	descrine "#index# do	
+		it "does something"
+		it "and somthing else"
+	end
+end
+
